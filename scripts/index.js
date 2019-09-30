085a91e16785b235f37a3f4b8eb3bb4aebfc488e
mihail <mihailpaslevel@gmail.com>
2019-09-30 13:04:09 +0300

    commit

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,7 @@ town.addEventListener('change',(event)=>{
 	fetch("https://openweathermap.org/data/2.5/weather?q="+town.value+",ua&appid=b6907d289e10d714a6e88b30761fae22",{method: 'GET'})
 					.then(res=>res.json())
 					.then(res=>{
+						show_result.innerHTML="";
 						show_result.innerHTML=
 							"Weather: "+res.weather[0].description+
 							"</br>Temperature: "+res.main.temp+
