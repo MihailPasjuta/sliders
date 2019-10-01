000b5b1c836dd500b57b6b96b20434fe27c735f2
mihail <mihailpaslevel@gmail.com>
2019-10-01 12:34:16 +0300

    commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,27 @@
 const town=document.getElementById('town');
-const show_result=document.getElementById('show_result');
+const show_result_categories=document.getElementById('show_result_categories');
+const show_result_information=document.getElementById('show_result_information');
 town.addEventListener('change',(event)=>{
 	fetch("https://openweathermap.org/data/2.5/weather?q="+town.value+",ua&appid=b6907d289e10d714a6e88b30761fae22",{method: 'GET'})
 					.then(res=>res.json())
 					.then(res=>{
-						show_result.innerHTML="";
-						show_result.innerHTML=
-							"Weather: "+res.weather[0].description+
-							"</br>Temperature: "+res.main.temp+
-							"</br>Wind: "+res.wind.speed+" m/s"+
-							"</br>Pressure: "+res.main.pressure+
-							"</br>Humidity: "+res.main.humidity;						
+						
+						show_result_categories.innerHTML=
+							"</br>Weather: <hr/>"
+							+"</br>Temperature: <hr/>"
+							+"</br>Wind: <hr/>"
+							+"</br>Pressure: <hr/>"
+							+"</br>Humidity: <hr/>"	
+							
+						show_result_information.innerHTML=
+							"</br>"+res.weather[0].description+"<hr/>"
+							+"</br>"+res.main.temp+" &deg;C<hr/>"
+							+"</br>"+res.wind.speed+" m/s<hr/>"
+							+"</br>"+res.main.pressure+"<hr/>"
+							+"</br>"+res.main.humidity+"<hr/>";
+							
+					})
+					.catch(res=>{
+						show_result_categories.innerHTML="Проверьте интернет соединение";
 					});
 });
